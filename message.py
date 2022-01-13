ebdf1d373497c6ca84322b279c8a345741c3bf1b
ljkjk <53215232+ljkjk@users.noreply.github.com>
2022-01-13 20:59:02 +0800

    Update message.py

--- message.py
+++ message.py
@@ -1,9 +1,10 @@
 import requests
 import json
 import os
-import datetime
+import time
 
-def dingtalk(msg, dingtalk_token):
+
+def dingtalk(msg, dingtalk_token, tries=5):
     dingtalk_url = 'https://oapi.dingtalk.com/robot/send?access_token='+dingtalk_token
     data = {
         "msgtype": "text",
@@ -16,12 +17,21 @@ def dingtalk(msg, dingtalk_token):
     }
     header = {'Content-Type': 'application/json'}
 
-    r = requests.post(dingtalk_url,
-                      data=json.dumps(data), headers=header).json()
-    return r["errcode"] == 0
+    for _ in range(tries):
+        try:
+            r = requests.post(dingtalk_url,
+                              data=json.dumps(data), headers=header).json()
+            print(r)
+            if r["errcode"] == 0:
+                return True
+        except:
+            pass
+        print('Retrying...')
+        time.sleep(5)
+    return False
 
 
-def pushplus(title, content, pushplus_token):
+def pushplus(title, content, pushplus_token, tries=5):
     title, content = title[:100], content[:100]
     title = '微信通知服务可能即将下线，请切换到其他通知通道（建议使用钉钉）\n' + title
     pushplus_url = 'http://pushplus.hxtrip.com/customer/push/send'
@@ -32,21 +42,39 @@ def pushplus(title, content, pushplus_token):
     }
     headers = {'Content-Type': 'application/json'}
 
-    r = requests.post(pushplus_url, data=json.dumps(data),
-                      headers=headers).json()
-    return r["code"] == 200
+    for _ in range(tries):
+        try:
+            r = requests.post(pushplus_url, data=json.dumps(data),
+                              headers=headers).text
+            print(r)
+            if '<code>200</code>' in r:
+                return True
+        except:
+            pass
+        print('Retrying...')
+        time.sleep(5)
+    return False
 
 
-def serverchan(text, desp, serverchan_key):
+def serverchan(text, desp, serverchan_key, tries=5):
     text, desp = text[:100], desp[:100]
     text = 'Server酱服务即将下线，请切换到其他通知通道（建议使用钉钉）\n' + text
-    r = requests.get("https://sc.ftqq.com/" + serverchan_key
-                     + ".send?text=" + text + "&desp=" + desp).json()
-    return r["errno"] == 0
+    for _ in range(tries):
+        try:
+            r = requests.get("https://sc.ftqq.com/" + serverchan_key
+                             + ".send?text=" + text + "&desp=" + desp).json()
+            print(r)
+            if r["errno"] == 0:
+                return True
+        except:
+            pass
+        print('Retrying...')
+        time.sleep(5)
+    return False
 
 
 if __name__ == "__main__":
-    msg = str(datetime.datetime.now()) + ' 打卡'
+    msg = "打卡"*1000
     dingtalk_token = os.environ.get('DINGTALK_TOKEN')
     if dingtalk_token:
         ret = dingtalk(msg, dingtalk_token)
